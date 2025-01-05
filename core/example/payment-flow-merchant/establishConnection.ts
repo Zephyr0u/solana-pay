e43276644503a1115ebc766336c7387481e039de
Zephyr <46925806+Zephyr0u@users.noreply.github.com>
2025-01-05 16:14:11 -0300

    Update establishConnection.ts

--- core/example/payment-flow-merchant/establishConnection.ts
+++ core/example/payment-flow-merchant/establishConnection.ts
@@ -9,6 +9,11 @@ export async function establishConnection(cluster: Cluster = 'devnet'): Promise<
     const connection = new Connection(endpoint, 'confirmed');
     const version = await connection.getVersion();
     console.log('Connection to cluster established:', endpoint, version);
+    export async function establishConnection(cluster: Cluster = 'devnet'): Promise<Connection> {
+    const endpoint = clusterApiUrl(cluster);
+    const connection = new Connection(endpoint, 'confirmed');
+    const version = await connection.getVersion();
+    console.log('Connection to cluster established:', endpoint, version);
 
     return connection;
 }
